7b8a42f532c9dbe3ba73af97880fcdb2eb7e92e6
Mikel Loidi Ardanaz <mloidi@MacBook-Pro-de-Mikel.local>
2019-07-14 17:40:26 +0200

    first option

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -1,54 +1,68 @@
 import React, { useContext } from 'react';
 
 import './Contacts.css';
-import { LanguageContext, ContactContext } from '../../globalState';
+import {
+  AuthContext,
+  LanguageContext,
+  ContactContext
+} from '../../globalState';
 import ContactCard from './ContactCard';
 import Icon from '../common/Icon';
 import NewContact from './NewContact';
 import ViewContact from './ViewContact';
 
 const Contacts = () => {
+  const { isAuthenticated, user } = useContext(AuthContext);
   const { getText } = useContext(LanguageContext);
   const {
     contacts,
-    load,
     showNewContact,
     setShowNewContact,
     showViewContact,
     showEditContact
   } = useContext(ContactContext);
-  // load();
-  // console.log(contacts);
+  if (isAuthenticated) {
+    console.log(contacts);
+  } else {
+    console.log('No token');
+  }
   return (
     <React.Fragment>
-      {showNewContact ? (
-        <NewContact />
-      ) : showViewContact ? (
-        <ViewContact />
-      ) : showEditContact ? (
-        <NewContact />
-      ) : (
+      {isAuthenticated ? (
         <React.Fragment>
-          <div className="App-contact-options">
-            <button
-              className="App-contact-button"
-              onClick={() => {
-                setShowNewContact(true);
-              }}
-            >
-              <Icon icon="faUserPlus" /> {' ' + getText('new')}
-            </button>
-          </div>
-          <div className="App-contact">
-            {/* {contacts &&
-              contacts.map(contact => (
-                <ContactCard
-                  key={contact._id ? contact._id : contact.id}
-                  contact={contact}
-                />
-              ))} */}
-          </div>
+          {showNewContact ? (
+            <NewContact />
+          ) : showViewContact ? (
+            <ViewContact />
+          ) : showEditContact ? (
+            <NewContact />
+          ) : (
+            <React.Fragment>
+              <div className="App-contact-options">
+                <button
+                  className="App-contact-button"
+                  onClick={() => {
+                    setShowNewContact(true);
+                  }}
+                >
+                  <Icon icon="faUserPlus" /> {' ' + getText('new')}
+                </button>
+                <span>{user.firstName}</span>
+              </div>
+              <div className="App-contact">
+                {contacts &&
+                  contacts.map(contact => (
+                    <ContactCard
+                      key={contact._id ? contact._id : contact.id}
+                      contact={contact}
+                    />
+                  ))}
+              </div>
+            </React.Fragment>
+          )}
         </React.Fragment>
+      ) : (
+        <div>Problem with the token</div>
       )}
     </React.Fragment>
   );
